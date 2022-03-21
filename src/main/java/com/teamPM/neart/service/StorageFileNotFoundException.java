96d8f935ca63a3b97ddd56d1779f50ef5b78113e
praybe <jungyjcool@gmail.com>
2022-03-21 21:20:18 +0900

    PRODUCT BACKEND UPDATE :art:

---
+++ src/main/java/com/teamPM/neart/service/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.teamPM.neart.service;
+
+public class StorageFileNotFoundException extends StorageException {
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
